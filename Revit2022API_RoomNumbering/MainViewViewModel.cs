123c68dd58ed7d668d8b302180146257ce0324a6
lllypik <blesphoton@gmail.com>
2022-12-03 14:31:33 +0300

    Добавьте файлы проекта.

---
+++ Revit2022API_RoomNumbering/MainViewViewModel.cs
@@ -0,0 +1,114 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revit2022API_RoomNumbering
+{
+    public class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+
+        public List<Level> Levels { get; }
+        public Level SelectedLevel { get; set; }
+
+        public DelegateCommand NumberingOnSelectedLevel { get; }
+        public DelegateCommand NumberingAll { get; }
+        public DelegateCommand CreateRoom { get; }
+
+        string numberString = string.Empty;
+        public string Prefix { get; set; }
+
+        private int numberBeginValue;
+        public int NumberBeginValue
+        {
+            get
+            {
+                return numberBeginValue;
+            }
+            set
+            {
+                try
+                {
+                    numberBeginValue = Convert.ToInt32(value);
+                }
+                catch (FormatException e)
+                {
+                    numberBeginValue = 1;
+                }
+            }        
+        }
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            Document doc = _commandData.Application.ActiveUIDocument.Document;
+
+            Levels = Utils.GetAllLevels(doc);
+
+            NumberingOnSelectedLevel = new DelegateCommand(OnNumberingOnSelectedLevel);
+            NumberingAll = new DelegateCommand(OnNumberingAll);
+            CreateRoom = new DelegateCommand(OnCreateRoom);
+        }
+
+        private void OnNumberingOnSelectedLevel()
+        {
+            Document doc = _commandData.Application.ActiveUIDocument.Document;
+
+            bool errorFlag = Utils.ChekOnNull(SelectedLevel, "Выберите уровень для расстановки нумерации");
+
+            List<Room> rooms = new List<Room>();
+            if (errorFlag == false)
+            {
+                rooms = Utils.FindRoomsOnlevel(doc, SelectedLevel);
+            }
+
+            errorFlag = Utils.ChekOnNull(rooms, "Не найдено созданных помещений на выбранном уровне.Разместите помещения на плане");
+
+            if (errorFlag == false)
+            {
+                Utils.SetNumber(doc, rooms, Prefix, numberBeginValue);
+            }
+        }
+
+        private void OnNumberingAll()
+        {
+            Document doc = _commandData.Application.ActiveUIDocument.Document;
+
+            List<Room> rooms = Utils.FindAllRooms(doc);
+
+            bool errorFlag = false;
+            errorFlag = Utils.ChekOnNull(rooms, "Не найдено созданных помещений. Разместите помещения на плане");
+
+            if (errorFlag == false)
+            {
+                Utils.SetNumber(doc, rooms, Prefix, numberBeginValue);
+            }
+        }
+
+        private void OnCreateRoom()
+        {
+            Document doc = _commandData.Application.ActiveUIDocument.Document;
+
+            bool errorFlag = Utils.ChekOnNull(SelectedLevel, "Выберите уровень для расстановки нумерации");
+
+            if (errorFlag == false)
+            {
+                Utils.CreateAllRoomOnLevel(doc, SelectedLevel);
+            }
+        }
+
+        public event EventHandler CloseReqest;
+
+        private void RaiseCloseReqest()
+        {
+            CloseReqest?.Invoke(this, EventArgs.Empty);
+        }
+
+   }
+}
---
+++ Revit2022API_RoomNumbering/Utils.cs
@@ -0,0 +1,126 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revit2022API_RoomNumbering
+{
+    public static class Utils
+    {
+        public static List<Level> GetAllLevels(Document document)
+        {
+            var levels = new FilteredElementCollector(document)
+                .OfClass(typeof(Level))
+                .OfType<Level>()
+                .ToList();
+
+            return levels;
+        }
+
+        public static List<Room> FindAllRooms(Document document)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(document);
+            collector.OfCategory(BuiltInCategory.OST_Rooms);
+            List<Room> rooms = new List<Room>();
+            foreach (Element element in collector)
+            {
+                Room room = element as Room;
+
+                if (room == null)
+                    continue;
+
+                rooms.Add(room);
+            }
+            return rooms;
+        }
+
+        public static void SetNumber(Document doc, List<Room> rooms, string prefix, int beginValue)
+        {
+            using (Transaction transaction = new Transaction(doc, "Numbering rooms"))
+            {
+                transaction.Start();
+
+                int numberValue = beginValue;
+                string numberString;
+
+                foreach (var room in rooms)
+                {
+                    numberString = prefix + numberValue;
+                    Parameter numberParameter = room.get_Parameter(BuiltInParameter.ROOM_NUMBER);
+                    numberParameter.Set(numberString);
+                    numberValue++;
+                }
+                transaction.Commit();
+            }
+        }
+
+        public static void CreateAllRoomOnLevel(Document doc, Level level)
+        {
+
+            using (Transaction transaction = new Transaction(doc, "Create rooms"))
+            {
+                transaction.Start();
+
+                PlanTopology pt = doc.get_PlanTopology(level);
+
+                foreach (PlanCircuit pc in pt.Circuits)
+                {
+                    if (!pc.IsRoomLocated)
+                    {
+                        Room room = doc.Create.NewRoom(null, pc);
+                    }
+                }
+
+                transaction.Commit();
+            }
+        }
+
+        public static List<Room> FindRoomsOnlevel(Document document, Level level)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(document);
+            collector.OfCategory(BuiltInCategory.OST_Rooms);
+
+            List<Room> allRooms = new List<Room>();
+
+            foreach (Element element in collector)
+            {
+                Room room = element as Room;
+
+                if (room == null)
+                    continue;
+
+                allRooms.Add(room);
+            }
+
+            List<Room> roomsOnLevel = allRooms
+                .Where(x => (x.LevelId == level.Id))
+                .ToList(); 
+
+            return roomsOnLevel;
+
+            //List<Room> rooms = new FilteredElementCollector(document)
+            //    .OfClass(typeof(Room))
+            //    .OfType<Room>()
+            //    .ToList();
+
+        }
+
+        public static bool ChekOnNull<t>(t value, string textError)
+        {
+            if (value == null)
+            {
+                TaskDialog.Show("Ошибка", textError);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
